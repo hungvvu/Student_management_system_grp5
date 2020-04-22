5bf739b606bf09be7d9e4c4b263827aba01c2bd4
krislam2001 <63941511+krislam2001@users.noreply.github.com>
2020-04-22 22:47:53 +0700

    View Profile

--- Student_Management_System/Student_Management_System/ShowMenu.cpp
+++ Student_Management_System/Student_Management_System/ShowMenu.cpp
@@ -10,14 +10,14 @@ int mainmenu(int& choice1)
 int MenuStudent(int& choice2)
 {
 	cout << "\n-----MENU------\n";
-	cout << "0.Quay Ve\n1.Attendance\n2.Xem Thoi Khoa Bieu\n3.Xem Diem\n4.Xem Bai Tap\n";
+	cout << "0.Quay Ve\n1.Attendance\n2.Xem Thoi Khoa Bieu\n3.Xem Diem\n4.Xem Bai Tap\n5.View Profile\n";
 	cin >> choice2;
 	return choice2;
 }
 int MenuStaff(int& choice2)
 {
 	cout << "\n-----MENU------\n";
-	cout << " 0.Return\n1.Manully Add Student to a class\n2.Edit an existing student\n3.Remove a student\n4.Change Student's Class\n5.View list of classes\n6.View list of students in a class\n";
+	cout << " 0.Return\n1.Manully Add Student to a class\n2.Edit an existing student\n3.Remove a student\n4.Change Student's Class\n5.View list of classes\n6.View list of students in a class\n7.View Profile\n";
 	cin >> choice2;
 	return choice2;
 }
@@ -25,7 +25,7 @@ int MenuStaff(int& choice2)
 int MenuLecturer(int& choice2)
 {
 	cout << "\n-----MENU------\n";
-	cout << " 0.Return\n1.Manully Add Student to a class\n2.Remove a student\3.Change Student's Class\n";
+	cout << " 0.Return\n1.Manully Add Student to a class\n2.Remove a student\3.Change Student's Class\n4.View Profile\n";
 	cin >> choice2;
 	return choice2;
 }
@@ -56,6 +56,10 @@ void showmenu(int& choice1, int& choice2, string Position)
 			{
 				showmenu(choice1, choice2, Position);
 			}
+			if (choice2 == 5)
+			{
+				Profile(username, password);
+			}
 		}
 		if (choice1 == 1)
 			if (Position == "Staff")
@@ -64,6 +68,10 @@ void showmenu(int& choice1, int& choice2, string Position)
 				while (choice2 == 0 && choice1 == 1)
 				{
 					showmenu(choice1, choice2, Position);
+				}
+				if (choice2 == 7)
+				{
+					Profile(username, password);
 				}
 			}
 		if (choice1 == 1)
@@ -74,6 +82,10 @@ void showmenu(int& choice1, int& choice2, string Position)
 				while (choice2 == 0 && choice1 == 1)
 				{
 					showmenu(choice1, choice2, Position);
+				}
+				if (choice2 == 4)
+				{
+					Profile(username, password);
 				}
 			}
 		}
--- Student_Management_System/Student_Management_System/main.cpp
+++ Student_Management_System/Student_Management_System/main.cpp
@@ -7,7 +7,10 @@ int main() {
 	choice2 = 0;
 	showmenu(choice1, choice2, Position);
 	if (choice2 == 1)
+	{
 		cout << "Success";//place to perform Attendance...
+	}
+	
 
 	return 0;
 }
