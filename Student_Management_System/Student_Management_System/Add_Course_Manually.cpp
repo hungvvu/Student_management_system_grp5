51a0234443da67ff76652da131dbb1a6378de391
krislam2001 <63941511+krislam2001@users.noreply.github.com>
2020-05-29 09:22:05 +0700

    Update Add_Course_Manually.cpp

--- Student_Management_System/Student_Management_System/Add_Course_Manually.cpp
+++ Student_Management_System/Student_Management_System/Add_Course_Manually.cpp
@@ -30,7 +30,9 @@ int AddCourseManually()
 
 	cout << "Enter Course ID: ";
 	cin >> newCourse.courseID;
+	cin.ignore();
 	cout << "Enter Course Name: ";
+	cin >> newCourse.courseName;
 	getline(cin, newCourse.courseName);
 	cout << "Enter Class: ";
 	cin >> newCourse.Class;
