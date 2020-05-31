e05679a1ffb5d24319ad66641158cdaa15d04f59
VietHung41 <63649200+VietHung41@users.noreply.github.com>
2020-06-01 02:57:59 +0700

    [Add_studentToCourse] Completed
    - Functional
    - Bug-free

--- Student_Management_System/Student_Management_System/Add_Stu_To_Course.cpp
+++ Student_Management_System/Student_Management_System/Add_Stu_To_Course.cpp
@@ -1,11 +1,12 @@
 #include "Declarations.h"
 
-void AddToFile(Stu S, ifstream& fin, FileSchedule Course) {
+void AddToFile(ifstream& fin, string OldFileName, Stu S, FileSchedule* Courses, int CourseIndex) {
 		ofstream temp;
 		temp.open("temp.txt");
 
 		if (!temp.is_open()) {
 			cout << "cannot create buffer" << endl;
+			return;
 		}
 		else {
 			int numofstu;// number of student
@@ -18,55 +19,57 @@ void AddToFile(Stu S, ifstream& fin, FileSchedule Course) {
 				temp << line << endl;
 			}
 
-			int d1 = stoi(s[j].startdateday);
-			int m1 = stoi(s[j].startdatemonth);
-			int y1 = stoi(s[j].startdateyear);
-			int d2 = stoi(s[j].enddateday);
-			int m2 = stoi(s[j].enddatemonth);
-			int y2 = stoi(s[j].enddateyear);
+			// print out the new student
+			int d1 = stoi(Courses[CourseIndex].startdateday);
+			int m1 = stoi(Courses[CourseIndex].startdatemonth);
+			int y1 = stoi(Courses[CourseIndex].startdateyear);
+			int d2 = stoi(Courses[CourseIndex].enddateday);
+			int m2 = stoi(Courses[CourseIndex].enddatemonth);
+			int y2 = stoi(Courses[CourseIndex].enddateyear);
 			int weekdays = 0;
-			if (s[j].dayofweek == "MON")
+			if (Courses[CourseIndex].dayofweek == "MON")
 			{
 				weekdays = 2;
 			}
-			if (s[j].dayofweek == "TUE")
+			if (Courses[CourseIndex].dayofweek == "TUE")
 			{
 				weekdays = 3;
 			}
-			if (s[j].dayofweek == "WED")
+			if (Courses[CourseIndex].dayofweek == "WED")
 			{
 				weekdays = 4;
 			}
-			if (s[j].dayofweek == "THUR")
+			if (Courses[CourseIndex].dayofweek == "THUR")
 			{
 				weekdays = 5;
 			}
-			if (s[j].dayofweek == "FRI")
+			if (Courses[CourseIndex].dayofweek == "FRI")
 			{
 				weekdays = 6;
 			}
-			if (s[j].dayofweek == "SAT")
+			if (Courses[CourseIndex].dayofweek == "SAT")
 			{
 				weekdays = 7;
 			}
-			Schedule << a[i].Fullname << endl;
-			Schedule << a[i].ID << endl;
-			Schedule << a[i].Password << endl;
-			Schedule << a[i].DoB << endl;
-			Schedule << "-1" << endl;//active and inactive
-			Schedule << "-1" << endl;
-			Schedule << "-1" << endl;
-			Schedule << "-1" << endl;
-			Schedule << "-1" << endl;
-			DateToFIle(Schedule, d1, d2, m1, m2, y1, y2, weekdays, s, j);
+			temp << S.Fullname << endl;
+			temp << S.ID << endl;
+			temp << S.Password << endl;
+			temp << S.DoB << endl;
+			temp << "-1" << endl;//active and inactive
+			temp << "-1" << endl;
+			temp << "-1" << endl;
+			temp << "-1" << endl;
+			temp << "-1" << endl;
+			DateToFIle(temp, d1, d2, m1, m2, y1, y2, weekdays, Courses, CourseIndex);
+			temp << endl;
 		}
 
 		temp.close();
 		fin.close();
 
-		remove("Student-19CLC1.txt");// remove old file
-		rename("temp.txt", "Student-19CLC1.txt");// rename the temp file
-		return 0;
+		remove(OldFileName.c_str());// remove old file
+		rename("temp.txt", OldFileName.c_str());// rename the temp file
+		cout << "added student " << S.ID << " to course " << Courses[CourseIndex].courseID << " successfully" << endl << endl;
 }
 
 void AddStu2Course() {
@@ -97,14 +100,22 @@ void AddStu2Course() {
 
 	bool found = false;
 	Stu S;
-	for (int i = 0; i < NumOfStu && !found; ++i) {
-		if (ID == Students[i].ID) {
-			found = true;
-			S = Students[i];
+	do {
+		for (int i = 0; i < NumOfStu && !found; ++i) {
+			if (ID == Students[i].ID) {
+				found = true;
+				S = Students[i];
+			}
 		}
-	}
+
+		if (!found) {
+			cout << "cannot find student with given ID, please re-enter: ";
+			cin >> ID;
+		}
+	} while (!found);
 	delete[] Students;
 
+	cout << "enter the course information" << endl;
 	string year;
 	string semester;
 	string CourseClass;
@@ -118,7 +129,7 @@ void AddStu2Course() {
 	cout << "enter course ID: ";
 	cin >> CourseID;
 
-	string StuFile = year + "-" + semester + "-" + Class + "-" + CourseID + "-Student.txt";
+	string StuFile = year + "-" + semester + "-" + CourseClass + "-" + CourseID + "-Student.txt";
 
 	ifstream fin;
 	fin.open(StuFile.c_str());
@@ -126,37 +137,31 @@ void AddStu2Course() {
 		cout << "cannot find the entered course" << endl;
 	}
 	else {
-		FileSchedule* Courses;
+		FileSchedule* Courses = NULL;
 		int n = 0;
-		n = CountCourse(fin);// count how many courses in the array
-		fin.seekg(0);// after count, the cursor is at the end of file, 
-					 // reset the read cursor to the beginning of the file
-
-		Courses = new FileSchedule[n];// count the number of course and alocate the needed memory
-
-		for (int i = 0; i < n; ++i) {// load the courses
-			getline(fin, Courses[i].No);
-			getline(fin, Courses[i].courseID);
-			getline(fin, Courses[i].courseName);
-			getline(fin, Courses[i].Class);
-			getline(fin, Courses[i].LUser);
-			getline(fin, Courses[i].LName);
-			getline(fin, Courses[i].Ldegree);
-			getline(fin, Courses[i].Lgender);
-			getline(fin, Courses[i].startdateday, ' ');
-			getline(fin, Courses[i].startdatemonth, ' ');
-			getline(fin, Courses[i].startdateyear, '\n');
-			getline(fin, Courses[i].enddateday, ' ');
-			getline(fin, Courses[i].enddatemonth, ' ');
-			getline(fin, Courses[i].enddateyear, '\n');
-			getline(fin, Courses[i].dayofweek);
-			getline(fin, Courses[i].starthour);
-			getline(fin, Courses[i].startminute);
-			getline(fin, Courses[i].endhour);
-			getline(fin, Courses[i].endminute);
-			getline(fin, Courses[i].Room);
-			fin.ignore();// ignore the empty line
+		
+		string ScheduleFile;
+		ScheduleFile = year + "-" + semester + "-Schedule-" + CourseClass + ".txt";
+		ifstream Load;
+		Load.open(ScheduleFile.c_str());
+		if (!Load.is_open()) {
+			cout << "cannot open schedule file" << endl;
 		}
+		else {
+			LoadCourse(Load, Courses, n);// load the course to an array
+			int CourseIndex = 0;
+			for (int i = 0; i < n; ++i) {
+				if (CourseID == Courses[i].courseID) {
+					CourseIndex = i;
+					break;
+				}
+			}
+			Load.close();
+
+			AddToFile(fin, StuFile, S, Courses, CourseIndex);
+		}
+		delete[] Courses;
+
 		fin.close();
 	}
 }
