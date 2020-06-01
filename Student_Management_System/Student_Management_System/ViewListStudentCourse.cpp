c3b4a469d42d0f701664636f34087aeb31df6327
krislam2001 <63941511+krislam2001@users.noreply.github.com>
2020-06-01 09:16:39 +0700

    View List Stu Course (part 2)

--- Student_Management_System/Student_Management_System/ViewListStudentCourse.cpp
+++ Student_Management_System/Student_Management_System/ViewListStudentCourse.cpp
@@ -1,6 +1,45 @@
 #include "Declarations.h"
 
-void ViewListStuCourse()
+int ViewListStuCourse()
 {
+	string f, AcaYear, Semester, Class, CourseID;
 
+	cout << "Enter Academic Year (yyyy-yyyy): "; cin.ignore(); getline(cin, AcaYear);
+	cout << "Enter Semester (HKn): "; getline(cin, Semester);
+	cout << "Enter Class: "; getline(cin, Class);
+	cout << "Enter Course ID: "; getline(cin, CourseID);
+
+	f = AcaYear + "-" + Semester + "-" + Class + "-" + CourseID + "-Student.txt";
+
+	ifstream fin;
+	fin.open(f.c_str());
+	int choice;
+	while (!fin)
+	{
+		cout << "INPUT ERROR !" << endl;
+		cout << "Press 0 to re-enter / Press 1 to return to menu: "; cin >> choice;
+		while (choice < 0 || choice > 1)
+		{
+			cout << "Press 0 to re-enter / Press 1 to return to menu: "; cin >> choice;
+		}
+		if (choice == 1)
+		{
+			return 1;
+		}
+		else
+		{
+			cout << "Enter Academic Year (yyyy-yyyy): "; cin.ignore(); getline(cin, AcaYear);
+			cout << "Enter Semester (HKn): "; getline(cin, Semester);
+			cout << "Enter Class: "; getline(cin, Class);
+			cout << "Enter Course ID: "; getline(cin, CourseID);
+
+			f = AcaYear + "-" + Semester + "-" + Class + "-" + CourseID + "-Student.txt";
+
+			fin.open(f.c_str());
+		}
+	}
+
+
+
+	fin.close();
 }
